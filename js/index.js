07bd0a11b4912903439ce03c5aea198b61fb7c7d
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-06-24 02:20:23 -0500

    Corresponding anchor of the current section highlights in nav bar

--- js/index.js
+++ js/index.js
@@ -11,7 +11,8 @@ const [ octText ] = octTexts;
 const [ project1, project2, project3, project4 ] = [ document.getElementById('project1'), document.getElementById('project2'), document.getElementById('project3'), document.getElementById('project4') ];
 const contactSub = document.getElementById('contactSub');
 const nav = document.getElementById('nav');
-const { offsetHeight:navH } = nav;
+const { offsetHeight:navH, children:anchors } = nav;
+const [ {offsetTop:canvasSec, offsetHeight:canvasH}, {offsetTop:aboutSec, offsetHeight:aboutH}, {offsetTop:skillsSec, offsetHeight:skillsH}, {offsetTop:projSec, offsetHeight:projH}, {offsetTop:contactSec, offsetHeight:contactH} ] = document.getElementsByClassName('section');
 const elems = [ aboutTitle, aboutBar, skillsTitle, skillsBar, projectTitle, projectBar, contactTitle, contactBar, oct, octText, octMe, meSummary, project1, project2, project3, project4, contactSub];
 
 
@@ -88,12 +89,31 @@ const addPop = (element) => {
 }
 
 
+const highLight = (anchor) => {
+	const {classList} = anchor;
+	if (!(classList.contains('current'))) {
+		const current = document.getElementsByClassName('current')[0];
+		if(current) {current.setAttribute('class', 'nonCurrent');}
+		classList.remove('nonCurrent');
+		classList.add('current');
+		
+	}
 
+}
 
 
 const checkPageY = () => {
 	
 	const pageY = window.pageYOffset;
+
+	
+	if (pageY >= canvasSec && pageY < canvasH + canvasSec) { highLight(anchors[0]);}
+	else if (pageY >= aboutSec && pageY < aboutH + aboutSec) { highLight(anchors[1]);}
+	else if (pageY >= skillsSec && pageY < skillsH + skillsSec) { highLight(anchors[2]);}
+	else if (pageY >= projSec && pageY < projH + projSec) { highLight(anchors[3]);}
+	else { highLight(anchors[4]);}
+
+
 
 	if (pageY >= (canvas.height + navH - 2)) {
 		addFixed();
@@ -101,6 +121,8 @@ const checkPageY = () => {
 	else if (pageY < canvas.height) {
 		removeFixed();
 	}
+
+
 
 	for (let elem of elems) {
 		
@@ -112,22 +134,24 @@ const checkPageY = () => {
 			if (elem === oct) {
 				octs.forEach(oct => addFlipInX(oct));
 			}
-			else if (elem === octText) {
+
+			else if (elem === octText) { 
 				octTexts.forEach(octText => addFadeIn(octText));
 			}
+
 			else if (elem === aboutTitle || elem === skillsTitle || elem === projectTitle || elem === contactTitle || elem === meSummary || elem === project2 || elem === project4 ) {
 				addSlideInLeft(elem);
 			}
+
 			else if (elem === contactSub) {
 				addPop(elem);
 			}
+
 			else {
 				addSlideInRight(elem);
 			}
-
 		}
 	}
-
 }
 
 
