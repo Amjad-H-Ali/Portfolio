6b58d461674995cc0eb7974e3f09babddb6afe76
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-07-07 10:38:34 -0500

    QuadTree generatespoint in random location

--- js/canvas.js
+++ js/canvas.js
@@ -1,12 +1,13 @@
 let boundary = new Rectangle(200, 200, 200, 200);
-let qt = new QuadTree(boundary);
+let qt = new QuadTree(boundary, 4);
+
+
+for (let i = 0; i < 1; i ++) {
+	let p = new Point(Math.random(), Math.random());
+	qt.insert(p);
+}
+
 console.log(qt);
-
-// for (let i = 0; i < 1; i ++) {
-// 	let p = new Point(random(width), random(height));
-// 	qt.insert(p);
-// }
-
 //Canvas
 const canvas = document.getElementById('canvas');
 //get Context
--- js/quadTree.js
+++ js/quadTree.js
@@ -12,11 +12,58 @@ class Rectangle {
 		this.w = w;
 		this.h = x;
 	}
+
+	contains(point) {
+		return (point.x > this.x - this.w &&
+			point.x < this.x + this.w &&
+			point.y > this.y - this.h &&
+			point.y < this.y + this.h
+		);
+	}
 }
 
 class QuadTree {
-	constructor(boundary) {
+	constructor(boundary, n) {
 		this.boundary = boundary;
-		// this.capacity = n;
+		this.capacity = n;
+		this.points = [];
+		this.divided = true;
+	}
+	subdivide() {
+		const {x, y, w, h} = this.boundary;
+		let ne = new Rectangle(x + w/2, y - h/2, w/2, h/2)
+		let nw = new Rectangle(x - w/2, y - h/2, w/2, h/2)
+		let se = new Rectangle(x + w/2, y + h/2, w/2, h/2)
+		let sw = new Rectangle(x - w/2, y + h/2, w/2, h/2)
+
+		this.northeast = new QuadTree(ne);
+		this.northwest = new QuadTree(nw);
+		this.southeast = new QuadTree(se);
+		this.southwest = new QuadTree(sw);
+		
+	}	
+	
+
+	insert(point) {
+
+		if (!this.boundary.contains(point)) {
+			return;
+		}
+
+		if (this.points.length < this.capacity) {
+			this.points.push(point);
+		} 
+		else {
+			if (!this.divided) {
+				this.subdivide();
+			}
+			
+			this.northeast.insert(point);
+			this.northwest.insert(point);
+			this.southeast.insert(point);
+			this.southwest.insert(point);
+
+		}
+
 	}
 }
